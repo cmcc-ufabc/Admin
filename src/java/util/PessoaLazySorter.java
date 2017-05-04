2ccc117b80384cba77070266cdd949393eecdba5
charles <charles.ferreira@aluno.ufabc.edu.br>
2017-05-04 12:57:12 -0300

    Remoção de linhas

--- src/java/util/PessoaLazySorter.java
+++ src/java/util/PessoaLazySorter.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package util;
 
 import java.lang.reflect.Field;
@@ -17,7 +11,6 @@ import org.primefaces.model.SortOrder;
  * @author charles
  */
 public class PessoaLazySorter implements Comparator<Pessoa> {
-
 
     private String sortField;
 
